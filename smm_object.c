82bdfa2bd2eb681cc90613d62cfcfa2fe5d08cd2
hsoob-in <tnqls2361@sookmyung.ac.kr>
2025-11-30 01:31:33 +0900

    13주차

--- smm_object.c
+++ smm_object.c
@@ -13,16 +13,10 @@
 #define MAX_NODETYPE    7
 #define MAX_GRADE       9
 
-#define SMMNODE_TYPE_LECTURE            0
-#define SMMNODE_TYPE_RESTAURANT         1
-#define SMMNODE_TYPE_LABORATORY         2
-#define SMMNODE_TYPE_HOME               3
-#define SMMNODE_TYPE_GOTOLAB            4
-#define SMMNODE_TYPE_FOODCHANCE         5
-#define SMMNODE_TYPE_FESTIVAL           6
+
 
 //node name expression
-static char smmNodeName[MAX_NODETYPE][MAX_CHARNAME] = {
+static char smmObj_NodeName[MAX_NODETYPE][MAX_CHARNAME] = {
        "lecture",
        "restaurant",
        "laboratory",
@@ -32,11 +26,20 @@ static char smmNodeName[MAX_NODETYPE][MAX_CHARNAME] = {
        "festival"
 };
 
-static int smm_nodeNr=0; // 몇 개의 노드 집합이 있는가 
-static char smm_name[MAX_NODENR][MAX_CHARNAME];
-static int smm_type[MAX_NODENR];
-static int smm_credit[MAX_NODENR];
-static int smm_energy[MAX_NODENR];
+static int smmObj_nodeNr=0; // 몇 개의 노드 집합이 있는가 
+
+//structure type definition
+
+typedef struct {
+    char name[MAX_CHARNAME];
+    int type;
+    int credit;
+    int energy; 
+} smmObj_board_t;
+
+//struct instance array definition
+static smmObj_board_t smmObj_board[MAX_NODENR];
+
 
 static int smm_foodNr=0; // 몇 개의 음식이 있는가 
 static char smm_food[MAX_NODENR][MAX_CHARNAME];
@@ -48,14 +51,14 @@ static char smm_festival[MAX_NODENR][MAX_CHARNAME];
 //object generation
 int smmObj_genNode(char* name, int type, int credit, int energy)
 {
-    strcpy(smm_name[smm_nodeNr],name);
-    smm_type[smm_nodeNr] = type;
-    smm_credit[smm_nodeNr] = credit;
-    smm_energy[smm_nodeNr] = energy;
+    strcpy(smmObj_board[smmObj_nodeNr].name, name);
+    smmObj_board[smmObj_nodeNr].type = type;
+    smmObj_board[smmObj_nodeNr].credit = credit;
+    smmObj_board[smmObj_nodeNr].energy = energy;
     
-    smm_nodeNr++; 
+    smmObj_nodeNr++; 
     
-    return (smm_nodeNr); //노드 몇개인지 반환 
+    return (smmObj_nodeNr); //노드 몇개인지 반환 
 }
 
 int smmObj_genFood(char* food, int foodenergy)
@@ -78,29 +81,36 @@ int smmObj_genFestival(char* festival)
 }
 
 //member retrieving
-char* smmObj_getName(int node_nr)
+char* smmObj_getNodeName(int node_nr)
 {
-      return (smm_name[node_nr]);
+      return (smmObj_board[node_nr].name);
 }
 
-int smmObj_getType(int node_nr)
+int smmObj_getNodeType(int node_nr)
 {
-      return (smm_type[node_nr]);
+      return (smmObj_board[node_nr].type);
 }
 
-char* smmObj_getTypeName(int node_nr)
+int smmObj_getNodeEnergy(int node_nr)
 {
-      return (smmNodeName[smm_type[node_nr]]);
+      return (smmObj_board[node_nr].energy);
+}
+
+int smmObj_getNodeCredit(int node_nr)
+{
+      return (smmObj_board[node_nr].credit);
+}
+
+
+char* smmObj_getTypeName(int node_type)
+{
+      return (smmObj_NodeName[node_type]);
 }
 
 
 
 #if 0
 //element to string
-char* smmObj_getNodeName(smmNode_e type)
-{
-    return smmNodeName[type];
-}
 
 char* smmObj_getGradeName(smmGrade_e grade)
 {
